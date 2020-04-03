a72b9ddf3bb19f26b28ac5e5e2c2e74b7b7acd88
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2020-04-03 09:48:48 -0400

    Unit tests

---
+++ inst/tinytest/test_RcppConcaveman.R
@@ -0,0 +1,35 @@
+data(points)
+
+# It works ?
+pts = points
+hull <- concaveman(pts)
+expect_equal(dim(hull), c(200,2))
+expect_equivalent(hull[1,], hull[200,])
+
+# It works with 3 points
+pts = points[1:3,]
+hull <- concaveman(pts)
+expect_equal(dim(hull), c(4,2))
+expect_equivalent(hull[1,], hull[4,])
+
+# It does not work with < 3 points
+pts = points[1:2,]
+expect_error(concaveman(pts))
+
+# It works with data.frame
+pts = as.data.frame(points)
+hull <- concaveman(pts)
+expect_equal(dim(hull), c(200,2))
+expect_equivalent(hull[1,], hull[200,])
+
+# It works with vectors
+x = points[,1]
+y = points[,2]
+hull <- concaveman(x, y)
+expect_equal(dim(hull), c(200,2))
+expect_equivalent(hull[1,], hull[200,])
+
+# Large concavity result in convex hull
+hull <- concaveman(points, concavity = 1e8)
+expect_equal(dim(hull), c(12,2))
+expect_equivalent(hull[1,], hull[12,])
